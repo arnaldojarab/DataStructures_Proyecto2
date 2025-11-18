afbb9a90dcd926a12b35dc6d9278f1f89787067c
KeylorSegura <keylor.segura.miranda@est.una.ac.cr>
2025-11-17 19:33:38 -0600

    removing details in a difficult_algorithm

--- src/game/enemy_logic/hard_algorithm.py
+++ src/game/enemy_logic/hard_algorithm.py
@@ -87,7 +87,7 @@ class HardAlgorithm:
         came_from = {}
         g_score = { (start_tx, start_ty): 0 }
 
-        # heurística: distancia Euclidiana
+        # heurística
         def h(tx, ty):
             dx = tx - goal_tx
             dy = ty - goal_ty
@@ -130,7 +130,7 @@ class HardAlgorithm:
                 py = ny * settings.TILE_SIZE
 
                 sw = self.map.surface_weight(px, py)
-
+                
                 # Penalización adicional si es parque
                 if self.map.is_park(nx, ny):
                     sw *= 0.3
@@ -163,3 +163,5 @@ class HardAlgorithm:
             path.append(current)
         path.reverse()
         return path
+    
+    
